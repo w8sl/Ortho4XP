0685a2273a8395126a7277ebd12df0aac4380e9b
w8sl <100048048+w8sl@users.noreply.github.com>
2023-08-19 12:27:22 +0200

    Update install_mac.sh
    Updated install script for Mac. More info as a comment inside.

--- install_mac.sh
+++ install_mac.sh
@@ -1,9 +1,14 @@
+# Install XCode Command Line Tools(enter the command: xcode-select --install   in the terminal)
+# Install Homebrew (download and run .pkg installer from https://github.com/Homebrew/brew/releases)
+# Verify integrity (enter the command: brew doctor  in the terminal), follow instructions if neccesary.
+
 # Install dependencies
-brew install python gdal spatialindex p7zip
+brew install gdal spatialindex p7zip python-tk
+
+# (python, proj, numpy should be already installed as a gdal dependencies. Verify with command: brew list --versions)
 
 # Install pyproj
-pip3 install cython
-pip3 install git+https://github.com/jswhit/pyproj.git
+pip3 install pyproj
 
 # Install other dependencies
-pip3 install numpy shapely rtree pillow requests
+pip3 install shapely rtree pillow requests
