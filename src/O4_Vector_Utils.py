83037e1860bc6694f32fcac31de94bfd8d7e67b3
w8sl <100048048+w8sl@users.noreply.github.com>
2023-09-03 15:02:49 +0200

    Fixed crash on step 1 (tiles with sparse OSM data)

--- src/O4_Vector_Utils.py
+++ src/O4_Vector_Utils.py
@@ -361,8 +361,12 @@ class Vector_Map():
                 (key,marker)=long_key
                 if key not in self.seeds: continue
                 for seed in self.seeds[key]:
-                    f.write(str(idx)+' '+' '.join(['{:.15f}'.format(s) for s in seed[0]])+' '+str(marker)+'\n')
-                    idx+=1
+                 if total_seeds==1:
+                  seed_value=seed
+                 else:
+                  seed_value=seed[0]
+                 f.write(str(idx)+' '+' '.join(['{:.15f}'.format(s) for s in seed_value])+' '+str(marker)+'\n')
+                 idx+=1
         f.close()
         return 
 ##############################################################################
