d646a5ab2d1716af8f40760246aa2a006c116777
w8sl <100048048+w8sl@users.noreply.github.com>
2023-10-11 19:16:38 +0200

    Install script for macOS 14

--- install_macOS14.sh
+++ install_macOS14.sh
@@ -6,6 +6,6 @@ brew install gdal python proj spatialindex p7zip python-tk
 
 pip3 install -r ./requirements_mac.txt
 
-# Rename files for OSX14 compatibility
+# Rename files for macOS 14 compatibility
 
-python3 ./OSX14_fix.py
+python3 ./macOS14_fix.py
