881172316b115ff804b1a8a5ed4a3194e4d07ca8
w8sl <100048048+w8sl@users.noreply.github.com>
2023-10-11 19:19:47 +0200

    Install script for macOS

--- install_mac.sh
+++ install_mac.sh
@@ -1,14 +1,7 @@
-# Install XCode Command Line Tools(open terminal, enter the command: xcode-select --install   →hit return)
-# Install Homebrew (open terminal, enter the command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"  →hit return.)
-# Verify integrity (enter the command: brew doctor  →hit return), read terminal output, follow recommendations if neccesary.
-# Copy paste line 8 and 14 into terminal or run install_mac.sh as a script (see Install_Instructions.txt).
-
 # Install dependencies
 
 brew install gdal python proj spatialindex p7zip python-tk
 
 # Install other dependencies
-# Go to the Ortho4XP folder: cd /path/to/Ortho4XP
-# or write in terminal: cd   →drag and drop Ortho4XP folder   →hit return
 
 pip3 install -r ./requirements_mac.txt
