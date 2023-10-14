c734628426f935fc71c544dd4d29c91ead22dbbf
w8sl <100048048+w8sl@users.noreply.github.com>
2023-10-14 18:31:21 +0200

    "yOrtho4XP_Overlays"  moves to ./build

--- src/O4_File_Names.py
+++ src/O4_File_Names.py
@@ -23,7 +23,7 @@ Patch_dir = os.path.join(Ortho4XP_dir, "Patches")
 Utils_dir = os.path.join(Ortho4XP_dir, "Utils")
 Tile_dir = os.path.join(Ortho4XP_dir, build_dir, "Tiles")
 Tmp_dir = os.path.join(Ortho4XP_dir, build_dir, "tmp")
-Overlay_dir = os.path.join(Ortho4XP_dir, "yOrtho4XP_Overlays")
+Overlay_dir = os.path.join(Ortho4XP_dir, build_dir, "yOrtho4XP_Overlays")
 
 ##############################################################################
 def short_latlon(lat,lon):
