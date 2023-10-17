ffc49427c8df0be0f9bb8ea2317641bff1a02f7c
w8sl <100048048+w8sl@users.noreply.github.com>
2023-10-17 06:19:05 +0200

    OSM overpass servers updated
    according to official list

--- src/O4_OSM_Utils.py
+++ src/O4_OSM_Utils.py
@@ -10,10 +10,9 @@ import O4_UI_Utils as UI
 import O4_File_Names as FNAMES
 
 overpass_servers={
-        "DE":"http://overpass-api.de/api/interpreter",
-        "FR":"http://overpass.openstreetmap.fr/oapi/interpreter",
-        "KU":"https://overpass.kumi.systems/api/interpreter",
-        "RU":"http://overpass.osm.rambler.ru/cgi/interpreter"
+         "DE":"https://overpass-api.de/api/interpreter",
+         "KU":"https://overpass.kumi.systems/api/interpreter",
+         "RU":"https://overpass.openstreetmap.ru/api/interpreter",
         }
 overpass_server_choice="DE"
 max_osm_tentatives=8
