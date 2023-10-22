a2a1c330c31e5c8413e4c64ba2f5516f38acfdda
w8sl <100048048+w8sl@users.noreply.github.com>
2023-10-22 12:13:59 +0200

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -4,7 +4,7 @@ brew install gdal python proj spatialindex p7zip python-tk
 
 # Install other dependencies
 
-pip3 install -r ./requirements_mac.txt
+pip3 install -r ./requirements.txt
 
 # Remove .app extension
 
