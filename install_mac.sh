0f4288b99383759d5bc133ba0299396c7de2b9be
w8sl <100048048+w8sl@users.noreply.github.com>
2023-12-04 09:41:51 +0100

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -5,7 +5,3 @@ brew install gdal python proj spatialindex p7zip python-tk
 # Install other dependencies
 
 pip3 install -r ./requirements.txt
-
-# Remove .app extension. This step is necessary on the latest version of macOS
-
-python3 ./Filename_fix_macOS.py
