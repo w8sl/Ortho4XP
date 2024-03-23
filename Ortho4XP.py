c7c80ee8a9ff1ac091559dd90a8f269495a0383d
w8sl <100048048+w8sl@users.noreply.github.com>
2024-03-23 10:45:23 +0100

    Merge "Progressive zl feature"

--- Ortho4XP.py
+++ Ortho4XP.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import os
-Ortho4XP_dir='..' if getattr(sys,'frozen',False) else '.'
-sys.path.append(os.path.join(Ortho4XP_dir,'src'))
+Ortho4XP_dir = os.pardir if getattr(sys, 'frozen', False) else os.curdir
+sys.path.extend([os.path.join(Ortho4XP_dir, 'src'),
+                 os.path.join(Ortho4XP_dir, 'Providers')])
 
+import O4_Airport_Data_Source as APT_SRC
 import O4_File_Names as FNAMES
 sys.path.append(FNAMES.Provider_dir)
 import O4_Imagery_Utils as IMG
@@ -15,32 +17,34 @@ import O4_GUI_Utils as GUI
 import O4_Config_Utils as CFG  # CFG imported last because it can modify other modules variables
 
 
-cmd_line="USAGE: Ortho4XP.py lat lon imagery zl (won't read a tile config)\n  OR:  Ortho4XP.py lat lon (with existing tile config file)"
+cmd_line="USAGE: Ortho4XP.py lat lon imagery zl (won't read a tile config)\n   OR:  Ortho4XP.py lat lon (with existing tile config file)"
 
 if __name__ == '__main__':
     if not os.path.isdir(FNAMES.Utils_dir):
         print("Missing ",FNAMES.Utils_dir,"directory, check your install. Exiting.")
-        sys.exit()   
+        sys.exit()
     for directory in (FNAMES.Preview_dir, FNAMES.Provider_dir, FNAMES.Extent_dir, FNAMES.Filter_dir, FNAMES.OSM_dir,
                       FNAMES.Mask_dir,FNAMES.Imagery_dir,FNAMES.Elevation_dir,FNAMES.Geotiff_dir,FNAMES.Patch_dir,
-                      FNAMES.Tile_dir,FNAMES.Tmp_dir):
+                      FNAMES.Tile_dir,FNAMES.Tmp_dir,FNAMES.Airport_dir):
         if not os.path.isdir(directory):
-            try: 
+            try:
                 os.makedirs(directory)
                 print("Creating missing directory",directory)
-            except: 
+            except:
                 print("Could not create required directory",directory,". Exit.")
                 sys.exit()
     IMG.initialize_extents_dict()
     IMG.initialize_color_filters_dict()
     IMG.initialize_providers_dict()
     IMG.initialize_combined_providers_dict()
+    APT_SRC.AirportDataSource.update_cache()
+
     if len(sys.argv)==1: # switch to the graphical interface
         Ortho4XP = GUI.Ortho4XP_GUI()
-
-        Ortho4XP.mainloop()	    
+        Ortho4XP.mainloop()
         print("Bon vol!")
-    else: # sequel is only concerned with command line 
+
+    else: # sequel is only concerned with command line
         if len(sys.argv)<3:
             print(cmd_line); sys.exit()
         try:
@@ -71,5 +75,12 @@ if __name__ == '__main__':
             print("Bon vol!")
         except:
             print("Crash!")
- 
-        
+
+    if False:
+        import json
+        import O4_Geo_Utils
+        print("04_airport_Data_Source.GTile :")
+        print(json.dumps(APT_SRC.GTile.cache_info(), indent=True))
+        print(json.dumps(APT_SRC.AirportCollection.cache_info(), indent=True))
+        for f in [O4_Geo_Utils.gtile_to_wgs84]:
+            print("{} : {}".format(f.__qualname__, f.cache_info()))
