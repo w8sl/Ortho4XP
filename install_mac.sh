ef706337977ecb4aaf49c9190d385e1b9bfe049d
w8sl <100048048+w8sl@users.noreply.github.com>
2024-05-17 20:29:53 +0200

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -5,7 +5,8 @@ brew install gdal python@3.11 proj spatialindex p7zip python-tk@3.11
 # Install other dependencies
 
 pip3.11 install -r ./requirements.txt
+pip3.11 install scikit-fmm
 
 # Install GDAL/Python bindings for Python 3.11
 
-pip3.11 install gdal=="$(gdal-config --version).*"
+pip3.11 install gdal==$(gdal-config --version)
