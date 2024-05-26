56555305d4f97d30a4a6b8f416ea535d7fb2a6b5
w8sl <100048048+w8sl@users.noreply.github.com>
2024-05-26 15:45:33 +0200

    Semi-automatic macOS venv install
    Run: chmod +x ./z_Install_O4XP_macOS_Python3.11_Venv.sh
    Then just drag-and-drop "z_Install_O4XP_macOS_Python3.11_Venv.sh" into terminal

--- z_Install_O4XP_macOS_Python3.11_Venv.sh
+++ z_Install_O4XP_macOS_Python3.11_Venv.sh
@@ -1,12 +1,36 @@
 #! /bin/bash
 
-#Uncomment the next line for a complete install on macOS
+if ! [ -x "$(command -v brew)" ]; then
+  echo 'Homebrew is required but is not installed or is not in PATH! See: https://brew.sh' >&2
+  exit 1
+fi
 
-#brew install gdal python@3.11 python-tk@3.11 proj spatialindex p7zip
+package_exists(){
+    package=$1
+    if brew list | grep $package; then
+        echo "$package found"
+    else
+        echo "$package not found"
+        echo "Installing $package"
+        brew install $package
+    fi
+}
+
+package_exists python@3.11
+package_exists gdal
+package_exists python-tk@3.11
+package_exists proj
+package_exists spatialindex
+package_exists p7zip
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 venv_path=$SCRIPT_DIR/venv-ortho
+
+if [ -d $venv_path ]; then
+  echo "Removing existing venv: $venv_path"
+  rm -rf $venv_path
+fi
 
 # 1. Create a Python virtual environment
 
@@ -36,9 +60,7 @@ echo "Installed packages:"
 echo " "
 pip list
 echo " "
-echo "GDAL version:"
-gdal-config --version
-echo " "
+echo "GDAL version: $(gdal-config --version)"
 echo " "
 echo "Use $SCRIPT_DIR/z_Start_O4XP_PythonVenv.sh to start O4XP"
 echo " "
