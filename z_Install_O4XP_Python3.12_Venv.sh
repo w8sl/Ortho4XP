cdf952b07af538bdfc7534dad169145c05262a48
w8sl <100048048+w8sl@users.noreply.github.com>
2024-05-28 16:37:53 +0200

    Semi-automated Python venv for macOS and Linux

--- z_Install_O4XP_Python3.12_Venv.sh
+++ z_Install_O4XP_Python3.12_Venv.sh
@@ -1,31 +1,102 @@
 #! /bin/bash
 
-#Uncomment the next line for a complete install on macOS
-
-#brew install gdal python python-tk proj spatialindex p7zip
-
-#On Linux - install system packages as per Install_Instructions.txt
-
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 venv_path=$SCRIPT_DIR/venv-ortho
 
-# 1. Create a Python 3.12 virtual environment
+if [ ! -f "$SCRIPT_DIR/Ortho4XP.py" ]; then
+  echo " "
+  echo "Error: file Ortho4XP.py not found!"
+  echo " "
+  echo "Place z_Install_O4XP_Python3.12_Venv.sh in the main O4XP direcory !"
+  echo " "
+  exit 1 
+fi
+
+# Semi-automated installation for macOS
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+   echo "macOS"
+
+ if ! [ -x "$(command -v brew)" ]; then
+   echo " "
+   echo "Homebrew is required but is not installed or is not in PATH!"
+   echo "Install Homebrew. Read the terminal messages carefully and follow the PATH setting instructions!"  
+   echo "See included Install_Instructions.txt and: https://brew.sh"
+   echo " "
+   exit 1
+ fi
+
+package_exists(){
+    package=$1
+    if brew list | grep $package; then
+        echo "$package found"
+    else
+        echo "$package not found"
+        echo "Installing $package"
+        brew install $package
+    fi
+}
+
+package_exists python@3.12
+string="$(which python3.12)"
+ if [[ $string != *"homebrew"* ]]; then
+   echo " "
+   echo "Python installed via Homebrew is required. Remove alternative Python from PATH!"
+   echo "See included Install_Instructtions.txt for more information"
+   echo " "
+   exit 1
+ fi
+package_exists gdal
+package_exists python-tk@3.12
+package_exists proj
+package_exists spatialindex
+package_exists p7zip
+
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+   echo "LINUX"
+   
+   if ! [ -x "$(command -v python3.12)" ]; then
+    echo " "
+    echo "Python 3.12 is not installed. Aborting! "
+    echo " "
+    exit 1
+   fi
+   if ! [ -x "$(command -v gdalwarp)" ]; then
+    echo " "
+    echo "Install system packages required by Ortho4XP as per included Install_Instructions.txt!"
+    echo " "
+    exit 1
+   fi
+
+else 
+  echo "Unsupported system!"
+  exit 1
+fi
+
+# Remove existing venv
+
+if [ -d $venv_path ]; then
+  echo "Removing existing venv: $venv_path"
+  rm -rf $venv_path
+fi
+
+# Create a Python 3.12 virtual environment
 
 python3.12 -m venv --system-site-packages $venv_path
 
-# 2. Activate Python venv
+# Activate Python venv
 
 source $venv_path/bin/activate
 
-# 3. Install packages with pip
+# Install packages with pip
 
 cd $SCRIPT_DIR
 
 pip install -I -r requirements.txt
 pip install build
 
-# 4. Download scikit-fmm's "meson" branch, compile and install it
+# Download scikit-fmm's "meson" branch, compile and install it
 
 git clone -b meson https://github.com/scikit-fmm/scikit-fmm.git $venv_path/scikit-fmm
 
@@ -33,7 +104,7 @@ python -m build $venv_path/scikit-fmm
 
 pip install $venv_path/scikit-fmm
 
-# 6. DONE
+# DONE
 
 echo " "
 
@@ -43,7 +114,6 @@ fi
 
 echo " "
 echo "Installed packages:"
-echo " "
 pip list
 echo " "
 echo "GDAL version:"
--- z_Install_O4XP_macOS_Python3.11_Venv.sh
+++ z_Install_O4XP_macOS_Python3.11_Venv.sh
@@ -1,8 +1,31 @@
 #! /bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+venv_path=$SCRIPT_DIR/venv-ortho
+
+if [ ! -f "$SCRIPT_DIR/Ortho4XP.py" ]; then
+  echo " "
+  echo "Error: file Ortho4XP.py not found!"
+  echo " "
+  echo "Place z_Install_O4XP_macOS_Python3.11_Venv.sh in the main O4XP direcory !"
+  echo " "
+  exit 1 
+fi
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+   echo "This script is for macOS only!"
+   exit 1
+fi
+
+# Semi-automated install for macOS
 if ! [ -x "$(command -v brew)" ]; then
-  echo 'Homebrew is required but is not installed or is not in PATH! See: https://brew.sh' >&2
-  exit 1
+   echo " "
+   echo "Homebrew is required but is not installed or is not in PATH!"
+   echo "Install Homebrew. Read the terminal messages carefully and follow the PATH setting instructions!"  
+   echo "See included Install_Instructions.txt and: https://brew.sh"
+   echo " "
+   exit 1
 fi
 
 package_exists(){
@@ -17,37 +40,43 @@ package_exists(){
 }
 
 package_exists python@3.11
+string="$(which python3.11)"
+if [[ $string != *"homebrew"* ]]; then
+  echo " "
+  echo "Python installed via Homebrew is required. Remove alternative Python from PATH!"
+  echo "See included Install_Instructtions.txt for more information"
+  echo " "
+  exit 1
+fi
 package_exists gdal
 package_exists python-tk@3.11
 package_exists proj
 package_exists spatialindex
 package_exists p7zip
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-venv_path=$SCRIPT_DIR/venv-ortho
+# Remove existing venv
 
 if [ -d $venv_path ]; then
   echo "Removing existing venv: $venv_path"
   rm -rf $venv_path
 fi
 
-# 1. Create a Python virtual environment
+# Create a Python virtual environment
 
 python3.11 -m venv $venv_path
 
-# 2. Activate Python venv
+# Activate Python venv
 
 source $venv_path/bin/activate
 
-# 3. Install packages with pip
+# Install packages with pip
 
 cd $SCRIPT_DIR
 pip install -r requirements.txt
 pip install gdal=="$(gdal-config --version).*"
 pip install scikit-fmm
 
-# 6. DONE
+# DONE
 
 echo " "
 
