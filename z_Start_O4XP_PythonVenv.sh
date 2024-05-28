5a8d36dfefedf97d64c922b9b96fcda0b0f83389
w8sl <100048048+w8sl@users.noreply.github.com>
2024-05-28 16:36:43 +0200

    Semi-automated Python3.12 venv for macOS and Linux

--- z_Start_O4XP_PythonVenv.sh
+++ z_Start_O4XP_PythonVenv.sh
@@ -3,6 +3,22 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 venv_path=$SCRIPT_DIR/venv-ortho
 
+if [ ! -f "$SCRIPT_DIR/Ortho4XP.py" ]; then
+  echo " "
+  echo "Error: file Ortho4XP.py not found!"
+  echo " "
+  echo "Place z_Start_O4XP_PythonVenv.sh in the main O4XP direcory !"
+  echo " "
+  exit 1 
+fi
+
+if [ ! -f $venv_path ]; then
+  echo " "
+  echo "Virtual environment not available. Install Python venv!"
+  echo " "
+  exit 1 
+fi
+
 cd $SCRIPT_DIR
 
 source $venv_path/bin/activate
