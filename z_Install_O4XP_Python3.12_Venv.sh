86c0887026cbe05ba18f5d6a93b871b5342e346d
w8sl <100048048+w8sl@users.noreply.github.com>
2024-05-31 21:47:05 +0200

    Guided Python venv installation

--- z_Install_O4XP_Python3.12_Venv.sh
+++ z_Install_O4XP_Python3.12_Venv.sh
@@ -13,16 +13,25 @@ if [ ! -f "$SCRIPT_DIR/Ortho4XP.py" ]; then
   exit 1 
 fi
 
-# Semi-automated installation for macOS
+# Semi-automated, guided installation for macOS
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
    echo "macOS"
 
+   if [[ "$(uname -m)" == "arm64" ]]; then
+     brew_path="/opt/homebrew/bin/brew"
+   else
+     brew_path="/usr/local/bin/brew"
+   fi
  if ! [ -x "$(command -v brew)" ]; then
    echo " "
    echo "Homebrew is required but is not installed or is not in PATH!"
-   echo "Install Homebrew. Read the terminal messages carefully and follow the PATH setting instructions!"  
-   echo "See included Install_Instructions.txt and: https://brew.sh"
+   echo " "
+   echo "Install Homebrew. See: https://brew.sh"
+   echo " "
+   echo "Add Homebrew to the PATH by running this command and restart the terminal:"
+   echo " "
+   echo "echo 'eval \"\$($brew_path shellenv)\"' >> /Users/$USER/.zprofile"
    echo " "
    exit 1
  fi
@@ -39,11 +48,19 @@ package_exists(){
 }
 
 package_exists python@3.12
-string="$(which python3.12)"
- if [[ $string != *"homebrew"* ]]; then
+
+ if [[ "$(which python3.12)" != *"homebrew"* ]]; then
    echo " "
-   echo "Python installed via Homebrew is required. Remove alternative Python from PATH!"
-   echo "See included Install_Instructtions.txt for more information"
+   echo "Python installed via Homebrew is required !" 
+   echo "Remove the PATH for Python in the hidden file .zprofile in your user directory !"
+   echo " "
+   echo "Contents of /Users/$USER/.zprofile: "
+   echo " "
+   echo "$(</Users/$USER/.zprofile )"
+   echo " "
+   echo "But it should be: "
+   echo " "
+   echo "eval \"\$($brew_path shellenv)\""
    echo " "
    exit 1
  fi
@@ -100,15 +117,14 @@ pip install -I -r requirements.txt
 echo " "
 
 if [ -d "$venv_path/bin" ]; then
-  echo "$(python --version) virtual environment has been created in $venv_path directory"
+  echo "$(python --version) venv has been created in the $venv_path directory"
 fi
 
 echo " "
 echo "Installed packages:"
 pip list
 echo " "
-echo "GDAL version:"
-gdal-config --version
+echo "GDAL version: $(gdal-config --version)"
 echo " "
 echo " "
 echo "Use $SCRIPT_DIR/z_Start_O4XP_PythonVenv.sh to start O4XP"
