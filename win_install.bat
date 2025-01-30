21338908311bc1edb68fe22a18eded77f351fa13
Sylvester S <100048048+w8sl@users.noreply.github.com>
2025-01-30 06:44:40 +0100

    Update to Python 3.12 and GDAL-3.10.1

--- win_install.bat
+++ win_install.bat
@@ -2,27 +2,27 @@
 
 cd /d %~dp0
 
-python --version | findstr "3.11" >nul
+python --version | findstr "3.12" >nul
 if %errorlevel% neq 0 (
-    echo Python 3.11 is not installed! 
-    echo Download and install Python3.11 from: https://www.python.org/downloads/windows/ ! 
+    echo Python 3.12 is not installed! 
+    echo Download and install Python3.12 from: https://www.python.org/downloads/windows/ ! 
     echo Customize installation, check the following options:
-    echo "Add python.exe to PATH", "pip", "tcl/tk and IDLE", "Add Python to environment variables", "Install Python 3.11 for all users"
+    echo "Add python.exe to PATH", "pip", "tcl/tk and IDLE", "Add Python to environment variables", "Install Python 3.12 for all users"
     pause
     exit /b 1)
         
-echo Python 3.11 is installed. Setting up Ortho4XP.
+echo Python 3.12 is installed. Setting up Ortho4XP.
 
 python -m venv venv
 
 call .\venv\Scripts\activate
 
-echo Installing GDAL from: https://github.com/cgohlke/geospatial-wheels/releases/tag/v2024.9.22
+echo Installing GDAL from: https://github.com/cgohlke/geospatial-wheels/releases/tag/v2025.1.20
 
-pip install https://github.com/cgohlke/geospatial-wheels/releases/download/v2024.9.22/GDAL-3.9.2-cp311-cp311-win_amd64.whl
+pip install https://github.com/cgohlke/geospatial-wheels/releases/download/v2025.1.20/GDAL-3.10.1-cp312-cp312-win_amd64.whl
 
 echo Installing requirements
-pip install -r requirements_win.txt
+pip install -r requirements.txt
 
 echo: 
 pip list
