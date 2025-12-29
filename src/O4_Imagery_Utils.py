a3c6ad4eba6f4ff90fecc5f58acdc8e105054f6f
Sylvester <100048048+w8sl@users.noreply.github.com>
2025-12-29 12:52:45 +0100

    Optimize build GeoTIFF script
    Add threading

--- src/O4_Imagery_Utils.py
+++ src/O4_Imagery_Utils.py
@@ -2594,6 +2594,7 @@ def r_translate(filein, fileout, r_crs, xmin, ymin, xmax, ymax):
                 "crs": r_crs,
                 "interleave": "pixel",
                 "jpeg_quality": 75,
+                "num_threads":4
             }
         )
 
@@ -2620,6 +2621,7 @@ def r_warp(filein, fileout, r_crs):
                 "compress": "jpeg",
                 "interleave": "pixel",
                 "jpeg_quality": 75,
+                "num_threads":4
             }
         )
 
