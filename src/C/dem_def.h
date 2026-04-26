37dd21abb7ed27d955e5d411e3e0f705c221df27
Sylvester <100048048+w8sl@users.noreply.github.com>
2026-04-26 19:52:38 +0200

    Add src/C/dem_def.h

---
+++ src/C/dem_def.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct DEM {
+	int epsg_code;
+	double tl, tr, bl, br;
+	uint32_t pixx, pixy;
+	uint8_t bpp;
+	uint16_t flags;
+	float scale, offset;
+	float *data;
+	void *encoded_data;
+};
