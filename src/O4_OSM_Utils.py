07c2647e3049439c2edc3140bd6ecb2001d3393e
Sylvester <100048048+w8sl@users.noreply.github.com>
2026-05-01 10:46:04 +0200

    Update OSM request header

--- src/O4_OSM_Utils.py
+++ src/O4_OSM_Utils.py
@@ -343,9 +343,10 @@ def OSM_query_to_OSM_layer(query,bbox,osm_layer,tags_of_interest=[],server_code=
 
 ##############################################################################
 def get_overpass_data(query,bbox,server_code=None):
+    s = requests.Session()
+    s.headers.update({"User-Agent": f"Ortho4XP-Progressive_130"})
     tentative=1
     while True:
-        s=requests.Session()
         true_server_code = server_code
         if not server_code:
            true_server_code = random.choice(list(overpass_servers.keys())) if overpass_server_choice=='random' else overpass_server_choice
